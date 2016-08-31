d7728ca26eb02029b0837c58b3c6f69d7982aa5c
RussBus24 <rjhenry1014@gmail.com>
2016-08-31 15:43:58 -0400

    fixed stackoverflow links

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ var getUnanswered = function(tags) {
 	};
 	
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		url: "https://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
@@ -105,7 +105,7 @@ function retrieveInspired(inspirationTag) {
 	};
 
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/tags/" + inspirationTag + "/top-answerers/all_time",
+		url: "https://api.stackexchange.com/2.2/tags/" + inspirationTag + "/top-answerers/all_time",
 		data: params,
 		dataType: "jsonp",
 		type: "GET",
